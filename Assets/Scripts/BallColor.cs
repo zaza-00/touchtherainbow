91d28423a39bc65f4b49e41deed81f7c3a9a5a4c
zaza-00 <2400973J@student.tp.edu.sg>
2025-08-15 01:16:33 +0800

    fixed!!!

--- Assets/Scripts/BallColor.cs
+++ Assets/Scripts/BallColor.cs
@@ -2,10 +2,55 @@ using UnityEngine;
 
 public class BallColor : MonoBehaviour
 {
-    public Color currentColor = Color.red; // Set in Inspector
+    public Color currentColor = Color.red; // Logical color
+    private SpriteRenderer sr;
+    private LineRenderer lr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        lr = GetComponent<LineRenderer>();
+
+        if (sr == null && lr == null)
+        {
+            Debug.LogWarning("No compatible renderer found on " + gameObject.name);
+        }
+
+        // Automatically pick up color from prefab if currentColor is default red
+        if (currentColor == Color.red)
+        {
+            if (sr != null)
+            {
+                currentColor = sr.color;
+            }
+            else if (lr != null)
+            {
+                currentColor = lr.startColor;
+            }
+        }
+
+        ApplyColor();
+    }
 
     public void SetColor(Color newColor)
     {
         currentColor = newColor;
+        ApplyColor();
+    }
+
+    private void ApplyColor()
+    {
+        if (sr != null)
+        {
+            Color visibleColor = currentColor;
+            visibleColor.a = 1f; // Ensure fully visible
+            sr.color = visibleColor;
+        }
+
+        if (lr != null)
+        {
+            lr.startColor = currentColor;
+            lr.endColor = currentColor;
+        }
     }
 }
--- Assets/Scripts/ColorMatchObstacle.cs
+++ Assets/Scripts/ColorMatchObstacle.cs
@@ -2,7 +2,45 @@ using UnityEngine;
 
 public class ColorMatchObstacle : MonoBehaviour
 {
-    public Color obstacleColor = Color.red; // Set in Inspector
+    public Color obstacleColor = Color.red; // Logical color
+    private SpriteRenderer sr;
+    private LineRenderer lr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        lr = GetComponent<LineRenderer>();
+
+        if (sr == null && lr == null)
+        {
+            Debug.LogWarning("No compatible renderer found on " + gameObject.name);
+        }
+
+        // Automatically pick up color from prefab if obstacleColor is default
+        if (obstacleColor == Color.red)
+        {
+            if (sr != null)
+            {
+                obstacleColor = sr.color;
+            }
+            else if (lr != null)
+            {
+                obstacleColor = lr.startColor;
+            }
+        }
+
+        // Apply the color to the renderers
+        if (sr != null)
+        {
+            sr.color = obstacleColor;
+        }
+
+        if (lr != null)
+        {
+            lr.startColor = obstacleColor;
+            lr.endColor = obstacleColor;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,11 +50,14 @@ public class ColorMatchObstacle : MonoBehaviour
 
             if (playerBall != null)
             {
-                // Compare colors by checking their distance
                 if (!ColorsMatch(playerBall.currentColor, obstacleColor))
                 {
                     Debug.Log("Wrong color! Player loses!");
-                    Destroy(other.gameObject); // Or trigger your game over
+                    Destroy(other.gameObject);
+                }
+                else
+                {
+                    Debug.Log("Correct color! You pass!");
                 }
             }
         }
@@ -24,10 +65,8 @@ public class ColorMatchObstacle : MonoBehaviour
 
     private bool ColorsMatch(Color a, Color b)
     {
-        // Allow a small tolerance in case of slight differences
-        float tolerance = 0.01f;
-        return Mathf.Abs(a.r - b.r) < tolerance &&
-               Mathf.Abs(a.g - b.g) < tolerance &&
-               Mathf.Abs(a.b - b.b) < tolerance;
+        return Mathf.Approximately(a.r, b.r) &&
+               Mathf.Approximately(a.g, b.g) &&
+               Mathf.Approximately(a.b, b.b);
     }
 }
